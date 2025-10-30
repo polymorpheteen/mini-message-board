bb8f98b8433c747f30dbd67ff95584d34e72cce2
polymorpheteen <138623315+polymorpheteen@users.noreply.github.com>
2025-10-30 22:51:45 +0100

    feat: add a Neon endpoint.

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
+require("dotenv").config();
+
 const express = require("express");
 const path = require("node:path");
 const app = express();
+const { Pool } = require("pg");
 const { indexRouter } = require("./routes/indexRouter");
 const newRouter = require("./routes/newRouter");
 const messageRouter = require("./routes/messageRouter");
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+
+const pool = new Pool({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: {
+    require: true,
+  },
+});
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -21,3 +37,15 @@ app.listen(PORT, (error) => {
   }
   console.log(`Server running at http://localhost:${PORT}`);
 });
+
+async function getPgVersion() {
+  const client = await pool.connect();
+  try {
+    const result = await client.query("SELECT version()");
+    console.log(result.rows[0]);
+  } finally {
+    client.release();
+  }
+}
+
+getPgVersion();
